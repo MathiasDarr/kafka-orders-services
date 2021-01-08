f90799d926602ddf51df2afe4ecebf4d7161beaf
mddarr <mddarr@gmail.com>
2021-01-07 19:02:30 -0800

    Trying to resolve the problems. orders-services/materialized-views

--- microservices/orders-services/materialized-views/src/main/java/org/mddarr/materializedviews/MaterializedViewsApplication.java
+++ microservices/orders-services/materialized-views/src/main/java/org/mddarr/materializedviews/MaterializedViewsApplication.java
@@ -1,14 +1,12 @@
-package org.mddarr.views.clean;
+package org.mddarr.materializedviews;
 
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.mddarr.products.AvroPurchaseCount;
-import org.mddarr.products.AvroPurchaseEvent;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,21 +14,23 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Map;
+import static org.apache.kafka.streams.StoreQueryParameters.fromNameAndType;
+import static org.apache.kafka.streams.state.QueryableStoreTypes.keyValueStore;
 
 @SpringBootApplication
 @EnableKafka
 @EnableKafkaStreams
-public class MaterializedViewApplication {
-
+public class MaterializedViewsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MaterializedViewApplication.class, args);
+		SpringApplication.run(MaterializedViewsApplication.class, args);
 	}
-
 }
 
 @Component
@@ -41,6 +41,21 @@ class OrderView {
 		builder.table("orders",
 				Consumed.with(Serdes.Integer(), Serdes.String()),
 				Materialized.as("orders-store"));
+	}
+}
+
+@Component
+@RequiredArgsConstructor
+class Producer {
+	
+
+	private final KafkaTemplate<Integer, String> kafkaTemplate;
+	@EventListener(ApplicationStartedEvent.class)
+	public void produce() {
+		kafkaTemplate.send("orders", 1, "iPad");
+		kafkaTemplate.send("orders", 2, "iPhone");
+		kafkaTemplate.send("orders", 1, "iPad, Airpods");
+		kafkaTemplate.send("orders", 2, "HomePod");
 	}
 }
 
---
+++ microservices/purchase-views/materialized-views/src/test/java/org/mddarr/materializedviews/MaterializedViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mddarr.materializedviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterializedViewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
